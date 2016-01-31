13d18b5be125a758806b318069464f3ffd27ee16
toyrobs <toyinrobinson@gmail.com>
2016-01-31 15:52:47 +0000

    Final version!

---
+++ k-poppers1.js
@@ -0,0 +1,14 @@
+var Tasks = new Mongo.Collection('tasks');
+ 
+if (Meteor.isClient) {
+ 
+  // This code only runs on the client
+  angular.module('simple-todos',['angular-meteor']);
+ 
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
+    function ($scope, $meteor) {
+ 
+      $scope.tasks = $meteor.collection(Tasks);
+ 
+    }]);
+}
